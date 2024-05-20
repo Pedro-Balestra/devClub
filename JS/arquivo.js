21b1e2fce16acbb0d4a908c1e2ef821f2dbd5908
Pedro <pedro.balestra@outlook.com>
2024-05-20 20:40:54 -0300

    Template Strings

---
+++ JS/arquivo.js
@@ -0,0 +1,7 @@
+const myAge = 30
+const myCar = "Si"
+
+const myString = `Minha idade é ${myAge}, ficando velho.
+Mas pelo menos estou andando de ${myCar}` // Template Literals / Template String
+
+console.log(myString);
