e8f5551f3c85a129c5c1722ce826ab36ea46abe4
Pedro <pedro.balestra@outlook.com>
2024-05-21 09:48:09 -0300

    Alterando textos & HTML no JavaScript

--- JS/arquivo.js
+++ JS/arquivo.js
@@ -1,4 +1,5 @@
-const elements = document.querySelectorAll("p")
+const elements = document.querySelector(".paragraph-js")
 
-console.log(elements);
-
+console.log(elements.textContent) // Só HTML
+console.log(elements.innerText) // Leva em conta o CSS
+console.log(elements.innerHTML) // tras tudo -> Pernite adicionar HTML
