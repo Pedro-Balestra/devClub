5b444bf6c7f796a2f42599d6bccf0c6bcc21f19b
Pedro <pedro.balestra@outlook.com>
2024-05-21 11:25:35 -0300

    DevClub Convert Money - HTML CSS

--- JS/arquivo.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector("#main-input")
-const select = document.querySelector("select")
-const button = document.querySelector(".main-button")
-
-function troqueiDeValor() {
-    console.log(`troquei de valor para ${select.value}`)
-}
-
-select.addEventListener("change", troqueiDeValor)
