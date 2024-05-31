7af4cbc69ca109d822d6aae39780f1c657daf8f9
Pedro <pedro.balestra@outlook.com>
2024-05-31 09:38:39 -0300

    Query Params

--- NODE/first-project-node/index.js
+++ NODE/first-project-node/index.js
@@ -3,8 +3,23 @@ const port = 3000
 
 const app = express()
 
+/*
+    - Query params => neusite.com/users?name=pedro&age=25 //Filtros
+    - Route params => /users/2 //Buscar, Deletar ou atualizar algo especifico
+*/
+
 app.get('/users', (request, response) => {
-    return response.send('Hello Node')
+    /*
+    const name = request.query.name
+    const age = request.query.age
+    */
+    const { name, age } = request.query  // Destructuring assignment
+    return response.json(
+        {
+            name,
+            age
+        }
+    )
 })
 
 app.listen(port, () => {
