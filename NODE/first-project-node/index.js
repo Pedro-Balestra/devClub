673adff69b8104065766a76b7f4c5c795e73e910
Pedro <pedro.balestra@outlook.com>
2024-05-31 10:18:13 -0300

    Middlewares

--- NODE/first-project-node/index.js
+++ NODE/first-project-node/index.js
@@ -13,9 +13,23 @@ app.use(express.json())
     - Post         => Cria informação no back-end
     - Put / Patch  => Alterar/Atualizar informação no back-end
     - Delete       => Deletar informação no back-end
+
+    - Middleware   => Interceptador => Tem o poder de parar ou alterar dados da requisição
 */
 
 const users = []
+
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+    const index = users.findIndex(user => user.id === id)
+    if (index < 0) {
+        return response.status(404).json({ error: 'User not found' })
+    }
+    request.userIndex = index
+    request.userId = id
+
+    next()
+}
 
 app.get('/users', (request, response) => {
     return response.json(
@@ -31,29 +45,23 @@ app.post('/users', (request, response) => {
     return response.status(201).json(user)
 })
 
-app.put('/users/:id', (request, response) => {
-    const { id } = request.params
+app.put('/users/:id', checkUserId, (request, response) => {
+
     const { name, age } = request.body
+    const index = request.userIndex
+    const id = request.userId
 
-    const updatedUser = { id, name, age }
+    const updatedUser = { id: id, name, age }
 
-    const index = users.findIndex(user => user.id === id)
-    if (index < 0) {
-        return response.status(404).json({ error: 'User not found' })
-    }
+
 
     users[index] = updatedUser
     return response.json(
         updatedUser)
 })
 
-app.delete('/users/:id', (request, response) => {
-    const { id } = request.params
-
-    const index = users.findIndex(user => user.id === id)
-    if (index < 0) {
-        return response.status(404).json({ error: 'User not found' })
-    }
+app.delete('/users/:id', checkUserId, (request, response) => {
+    const index = request.userIndex
 
     users.splice(index, 1)
     return response.status(204).send()
