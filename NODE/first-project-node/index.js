06a43de2371405070d173cce32cd799e5eaaa537
Pedro <pedro.balestra@outlook.com>
2024-05-31 10:07:00 -0300

    Primeiro projeto Node

--- NODE/first-project-node/index.js
+++ NODE/first-project-node/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const uuid = require('uuid')
 const port = 3000
 
 const app = express()
@@ -7,43 +8,60 @@ app.use(express.json())
 /*
     - Query params => neusite.com/users?name=pedro&age=25 //Filtros
     - Route params => /users/2 //Buscar, Deletar ou atualizar algo especifico
+
+    - Get          => Buscar informação no back-end
+    - Post         => Cria informação no back-end
+    - Put / Patch  => Alterar/Atualizar informação no back-end
+    - Delete       => Deletar informação no back-end
 */
 
+const users = []
+
 app.get('/users', (request, response) => {
+    return response.json(
+        users)
+})
+
+app.post('/users', (request, response) => {
     const { name, age } = request.body
-    return response.json(
-        {
-            name,
-            age
-        })
-})
-
-/*
-app.get('/users/:id', (request, response) => {
-
-    const { id } = request.params
-    return response.json(
-        {
-            id
-        }
-    )
-})
-* /
-/*
-app.get('/users', (request, response) => {
-
-    const name = request.query.name
-    const age = request.query.age
-    
-const { name, age } = request.query  // Destructuring assignment
-return response.json(
-    {
-        name,
-        age
+    const user = {
+        id: uuid.v4(), name, age
     }
-)
+    users.push(user)
+    return response.status(201).json(user)
 })
-*/
+
+app.put('/users/:id', (request, response) => {
+    const { id } = request.params
+    const { name, age } = request.body
+
+    const updatedUser = { id, name, age }
+
+    const index = users.findIndex(user => user.id === id)
+    if (index < 0) {
+        return response.status(404).json({ error: 'User not found' })
+    }
+
+    users[index] = updatedUser
+    return response.json(
+        updatedUser)
+})
+
+app.delete('/users/:id', (request, response) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+    if (index < 0) {
+        return response.status(404).json({ error: 'User not found' })
+    }
+
+    users.splice(index, 1)
+    return response.status(204).send()
+})
+
+
+
+
 
 app.listen(port, () => {
     console.log(`🚀Server is running on port ${port}`)
