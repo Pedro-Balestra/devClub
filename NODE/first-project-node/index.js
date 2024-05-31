bbebadc7b979bfa355399b6b6bc534bf99b89d67
Pedro <pedro.balestra@outlook.com>
2024-05-31 09:41:59 -0300

    Route Params

--- NODE/first-project-node/index.js
+++ NODE/first-project-node/index.js
@@ -8,19 +8,30 @@ const app = express()
     - Route params => /users/2 //Buscar, Deletar ou atualizar algo especifico
 */
 
-app.get('/users', (request, response) => {
-    /*
-    const name = request.query.name
-    const age = request.query.age
-    */
-    const { name, age } = request.query  // Destructuring assignment
+app.get('/users/:id', (request, response) => {
+
+    const { id } = request.params
     return response.json(
         {
-            name,
-            age
+            id
         }
     )
 })
+/*
+app.get('/users', (request, response) => {
+
+    const name = request.query.name
+    const age = request.query.age
+    
+const { name, age } = request.query  // Destructuring assignment
+return response.json(
+    {
+        name,
+        age
+    }
+)
+})
+*/
 
 app.listen(port, () => {
     console.log(`🚀Server is running on port ${port}`)
