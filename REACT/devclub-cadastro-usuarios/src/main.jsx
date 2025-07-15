3e4282e4e6c78320a5081a2651bae9ce3990fcd0
Pedro Balestra <pedro.balestra@outlook.com>
2025-07-15 17:39:57 -0300

    17 - Entendendo o conteito de rotas no react

--- REACT/devclub-cadastro-usuarios/src/main.jsx
+++ REACT/devclub-cadastro-usuarios/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Home } from "./pages/Home";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/routes.jsx";
 import { GlobalStyles } from "./styles/GlobalStyles.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyles />
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ REACT/devclub-cadastro-usuarios/src/pages/ListUsers/index.jsx
@@ -0,0 +1,7 @@
+export function ListUsers() {
+  return (
+    <div>
+      <h1>ListUsers</h1>
+    </div>
+  );
+}
