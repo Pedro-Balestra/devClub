4c8277f474d055c40f85bce499ec3437d299c6c9
Pedro Balestra <pedro.balestra@outlook.com>
2025-07-16 15:24:02 -0300

    23 - useEffect

--- REACT/devclub-cadastro-usuarios/src/pages/ListUsers/index.jsx
+++ REACT/devclub-cadastro-usuarios/src/pages/ListUsers/index.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { DefaultButton } from "../../components";
 import { DefaultBackGround } from "../../components/BackGround";
+import api from "../../services/api";
 
 export function ListUsers() {
   const navigate = useNavigate();
+
+  // Toda vez que a tela carrega,o useEffect é executado
+  // Toda vez que uma determinada variável é alterada, o useEffect é executado
+  useEffect(() => {
+    async function getUsers() {
+      const data = await api.get("/usuarios");
+      console.log(data);
+    }
+    getUsers();
+  }, []);
   return (
     <div>
       <DefaultBackGround>
